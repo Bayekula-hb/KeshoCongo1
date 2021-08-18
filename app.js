834e03aa33880e6323443a0e59e2d76078a93616
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-18 11:56:46 +0100

    feat install mysql

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
-
+const sequelize = require("./models/index");
 const app = express();
 const cors = require("cors");
 
+(async()=>{try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}})()
 app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+  res.json({ message: "Votre requête a bien été reçue !" });
+});
 
 module.exports = app;
