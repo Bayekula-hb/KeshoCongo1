2f8bbea465ad85ec485b55720b722b16bf5d9764
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-18 15:20:21 +0100

    feat install bcrypt and jsonwebtoken

--- utils/Dbconnect.js
+++ utils/Dbconnect.js
@@ -1,15 +1,12 @@
 const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize("atelier_sequelize", "root", "hobed", {
-// 	dialect: "mysql",
-// 	host: "localhost"
-// });
-
-// const sequelize = new Sequelize("bsouruivlyra7oak70av","ugkdeyce9bmzpgij","SvRh1B1Z9eZqQrzZS59a",{
-//     dialect: "mysql",
+// const db = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD,{
+//     host:process.env.HOST,
+//     dialect:"mysql"
 // })
-const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD,{
-    host:process.env.HOST,
-    dialect:"mysql"
-})
-module.exports = sequelize;
+const db = new Sequelize("kesho-congo", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+module.exports = db;
