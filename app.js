ce9253107207ff6605625645ecf5a9ac45d195e6
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-18 11:04:04 +0100

    feat install cors

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
 const app = express();
+const cors = require("cors");
 
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' }); 
+ });
 
 module.exports = app;
